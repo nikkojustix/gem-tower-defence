dcd350617f094f09ce04ff5dbc240034579b383e
nikkojustix <nickolaykotov@gmail.com>
2022-01-17 16:52:18 +0300

    added custom font

--- src/Hud.js
+++ src/Hud.js
@@ -1,9 +1,10 @@
 import Phaser from "phaser"
-import btn from './assets/UI/Wenrexa Assets GUI Dark Miko/Standart Button V1/Standart Button Normal/standart button normal 3.png'
-import btnHover from './assets/UI/Wenrexa Assets GUI Dark Miko/Standart Button V1/Standart Button Hover/standart button hover 3.png'
-import btnActive from './assets/UI/Wenrexa Assets GUI Dark Miko/Standart Button V1/Standart Button Active/standart button active 3.png'
+import bg from './assets/UI/bg.png'
+import btn from './assets/UI/btn.png'
+import btnActive from './assets/UI/btn_pressed.png'
+import btnDisabled from './assets/UI/btn_disabled.png'
 
-const FONT_STYLE = { align: 'center', fontSize: '18px', fontFamily: 'Arial', fontWeight: 'bold', }
+const FONT_STYLE = { fontSize: '18px', fontFamily: 'KenVector', color: '#88E060', }
 
 class Hud extends Phaser.Scene {
   constructor() {
@@ -11,14 +12,16 @@ class Hud extends Phaser.Scene {
   }
 
   preload() {
+    this.load.image('bg', bg)
     this.load.image('btn', btn)
-    this.load.image('btnHover', btnHover)
     this.load.image('btnActive', btnActive)
+    this.load.image('btnDisabled', btnDisabled)
   }
 
   create() {
-    const buildBtn = this.add.image(940, 50, 'btn').setOrigin(0).setInteractive({ useHandCursor: 'true' })
-    const buildText = this.add.text(940, 50, 'BUILD', FONT_STYLE).setPadding(30, 10)
+    this.add.image(900, 0, 'bg').setOrigin(0)
+    const buildBtn = this.add.image(930, 26, 'btn').setOrigin(0).setInteractive({ useHandCursor: 'true' })
+    const buildText = this.add.text(972, 36, 'BUILD', FONT_STYLE,)
     const removeBtn = this.add.image(940, 120, 'btn').setOrigin(0).setInteractive({ useHandCursor: 'true' })
     const removeText = this.add.text(960, 129, 'REMOVE', FONT_STYLE)
 
@@ -29,17 +32,19 @@ class Hud extends Phaser.Scene {
     })
 
 
-    this.input.on('gameobjectover', (pointer, gameObject) => {
-      gameObject.setTexture('btnHover')
-    })
-    this.input.on('gameobjectout', (pointer, gameObject) => {
-      gameObject.setTexture('btn')
-    })
+    // this.input.on('gameobjectover', (pointer, gameObject) => {
+    //   gameObject.setTexture('btnHover')
+    // })
+    // this.input.on('gameobjectout', (pointer, gameObject) => {
+    //   gameObject.setTexture('btn')
+    // })
     this.input.on('gameobjectdown', (pointer, gameObject) => {
-      buildBtn.setTexture('btnActive')
+      gameObject.setTexture('btnActive').setPosition(gameObject.getTopLeft().x, gameObject.getTopLeft().y + 4)
+      gameObject.setPosition(gameObject.getTopLeft().x, gameObject.getTopLeft().y + 4)
     })
     this.input.on('gameobjectup', (pointer, gameObject) => {
-      buildBtn.setTexture('btn')
+      gameObject.setTexture('btn').setPosition(gameObject.getTopLeft().x, gameObject.getTopLeft().y - 4)
+      gameObject.setPosition(gameObject.getTopLeft().x, gameObject.getTopLeft().y - 4)
     })
 
   }
